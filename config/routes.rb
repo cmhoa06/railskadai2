1d54a2dc139e35b9cafda350fb3c1500232057e1
Moe Nakajima <muskbergamot@gmail.com>
2023-03-30 23:32:14 +0900

    各モデル、コントローラ作成、root設定、ユーザー関連

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :reservations
+  devise_for :rooms
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :reservations
+  resources :rooms
+  resources :users
 end
